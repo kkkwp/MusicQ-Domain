7b79d40d88127f9a3a759b0bf1e828e147f493cf
SollyJ <jeongpine14@gmail.com>
2023-04-21 11:14:00 +0900

    :card_file_box:db: 음악 데이터 추가 [DRKKO-124] (#16)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,7 +7,7 @@ values ("You and me
 한참을 쳐다봐
 가까이 다가가
 You see
-You see ey ey ey ey
+You see
 One two three
 용기가 생겼지
 이미 아는 네 눈치
@@ -17,7 +17,7 @@ You see
 여기 보이니
 Looking for attention 너야겠어
 확실하게 나로 만들겠어
-Stop eyyy
+Stop
 Drop the question
 Drop the drop the question
 Want attention
@@ -43,7 +43,7 @@ Hey 다 들켰었나
 널 보면 하트가 튀어나와
 난 사탕을 찾는 baby
 내 맘은 설레이지
-Eyyy drop the question
+drop the question
 Drop the, drop the question
 Want attention
 Wanna want attention
@@ -100,23 +100,23 @@ Hype boy 너만 원해
 Hype boy 내가 전해
 And we can go high
 말해봐 yeah
-느껴봐 mm mm
+느껴봐
 Take him to the sky
 You know I hype you boy
 눈을 감아
 말해봐 yeah
-느껴봐 mm mm
+느껴봐
 Take him to the sky
 You know I hype you boy
 잠에 들려고 잠에 들려 해도
-네 생각에 또 새벽 세 시 uh oh
+네 생각에 또 새벽 세 시
 알려줄 거야 they cant have you no more
 봐봐 여기 내 이름 써있다고
 누가 내게 뭐라든
 남들과는 달라 넌
 Maybe you could be the one
 날 믿어봐 한번
-Im not looking for just fun
+I am not looking for just fun
 Maybe I could be the one
 Oh baby
 예민하대 나 lately
@@ -126,84 +126,68 @@ I just want you
 Call my phone right now
 I just wanna hear you are mine
 Cause I know what you like boy
-You''re my chemical hype boy
+You are my chemical hype boy
 내 지난날들은
 눈 뜨면 잊는 꿈
 Hype boy 너만 원해
 Hype boy 내가 전해
 And we can go high
 말해봐 yeah
-느껴봐 mm mm
+느껴봐
 Take him to the sky
 You know I hype you boy
 눈을 감아
 말해봐 yeah
-느껴봐 mm mm
+느껴봐
 Take him to the sky
 You know I hype you boy", 2);
 
--- Zior Park - CHRISTIAN
+-- IVE - I AM
 insert into music(music_title, singer, video_id)
-values ('CHRISTIAN', 'Zior Park', 'IZFPv2fixxc');
+values ('I AM', 'IVE', '6ZUIwj3FgUY');
 insert into lyrics(music_lyrics, music_id)
-values ("Money makes me feel better
-You told me it was a ticket to hell ok how come?
-Do you think that Im a devil?
-Sex makes me feel better how could you judge me?
-I know you dont deserve to
-Now You act like a Pharisee
-When I was poor my mom was stressed out
-Now look money made me a good boy to my mom
-When I was poor I was like a hungry fox
-Now look I saved a lotta buddies from the basement
-I just bought a Christian its blinging on my body
-Im toasting with celebrities at the same bar
-Hey pretty do you wanna fuck with me? Thats no problem
-But Sunday morning is coming I gotta go to church
-Im still fucking christian
-Though Im wearing new Christian
-You should check my algorithm
-Its not much different from the old me
-Im still fucking christian
-Though Im wearing new Christian
-You should check my algorithm
-Its not much different from the old me
-Im still fucking christian
-Im still fucking christian
-Im still fucking
-Stinky smells stuck in my room
-Even after I took a shower
-How can I erase it
-Wheres the origin ?
-Hey Mr Im talking about you
-Whats funny? Seriously?
-You feel this disgusting smell
-I think its from your stinky breath
-Street people you think you are smart
-You think you got no god
-But I know you worship yourself
-What a stupid god Is your god a liar?
-Your god is so outrageous you would better die
-I just bought a Christian its blinging on my body
-Im toasting with celebrities at the same bar
-Hey pretty do you wanna fuck with me? Thats no problem
-But Sunday morning is coming I gotta go to church
-Im still fucking christian
-Though Im wearing new Christian
-You should check my algorithm
-Its not much different from the old me
-Im still fucking christian
-Though Im wearing new Christian
-You should check my algorithm
-Its not much different from the old me
-Im still fucking christian
-Im still fucking christian
-Im still fucking
-Oh lord forgive my people I didnt know what Ive done
-Oh lord Look at my finger dirt is stuck in my nails
-Oh lord please clean my tongue its covered by pukes and phlegms
-Oh mom now I get it why you told me that you wanna go back when I was a child
-Im still fucking christian", 3);
+values ("다른 문을 열어 따라 갈 필요는 없어
+넌 너의 길로 난 나의 길로 음
+하루하루마다 색이 달라진 느낌
+밝게 빛이 나는 길을 찾아 I am on my way 넌 그냥 믿으면 돼
+I am on my way 보이는 그대로야
+너는 누군가의 Dreams come true
+제일 좋은 어느 날의 데자뷰
+머물고픈 어딘가의 낯선 뷰
+I all be far away
+That's my Life is 아름다운 갤럭시
+Be a writer 장르로는 판타지
+내일 내게 열리는 건 big big 스테이지
+So that is who I amLook at me now
+어제랑 또 다른 짜릿한 나
+두려운 모든 게 설레이게
+I am in sky high OMG
+사소한 건 다 아득해져 와
+Look at me now
+I am on fire
+I am on my way 넌 그냥 믿으면 돼
+I am on my way 보이는 그대로야
+너는 누군가의 Dreams come true
+제일 좋은 어느 날의 데자뷰
+머물고픈 어딘가의 낯선 뷰
+I will be far away
+Thats my Life is 아름다운 갤럭시
+Be a writer 장르로는 판타지
+내일 내게 열리는 건 big big 스테이지
+So that is who I am
+어느 깊은 밤 길을 잃어도
+차라리 날아올라 그럼 네가
+지나가는 대로 길이거든
+one two three
+Fly up
+I hope you be someones dreams come true
+제일 좋은 어느 날의 데자뷰
+머물고픈 어딘가의 낯선 뷰
+I will be far away
+Thats my Life is 아름다운 갤럭시
+Be a writer 장르로는 판타지
+내일 내게 열리는 건 big big 스테이지
+So that is who I am", 3);
 
 -- DPR IAN - Calico
 insert into music(music_title, singer, video_id)
@@ -221,7 +205,7 @@ Underneath the midnight coal
 자정 숯불 아래
 Im not your lover
 너의 연인이 아니야
-Im just another shhhh
+Im just another
 나는 그냥 또 하나에 쉿
 My hearts in Calico
 내 마음은 칼리코에 있어
@@ -233,7 +217,7 @@ You dont want to be alone
 너는 혼자 있고 싶지 않아
 My heart is lost in Calico
 내 마음은 칼리코에서 잃어버렸어
-Oh girl youll never know
+Oh girl you will never know
 Oh girl 너는 절대 모를 거야
 Its jst another show
 그냥 또 하나의 쇼일뿐
@@ -307,7 +291,7 @@ You locked yourself in this imagination
 넌 이 망상 속에 자신을 스스로 가뒀어
 And as we draw our goodbyes
 우리들의 마지막을 그리며
-Ill be patient
+I will be patient
 침착하게 기다리고 있을 게
 For your words
 너의 말들을
@@ -351,3 +335,445 @@ I thought you would be okay
 너가 괜찮을 줄 알았어
 You thought I would be the same
 너는 내가 똑같을 거라 생각했겠지", 4);
+
+-- 원더걸스 - Tell me
+insert into music(music_title, singer, video_id)
+values ('Tell me', '원더걸스', 'BlHv3BbBv6A');
+insert into lyrics(music_lyrics, music_id)
+values ("너도 날 좋아할 줄은 몰랐어
+어쩌면 좋아 너무나 좋아
+꿈만 같아서 나
+내 자신을 자꾸 꼬집어 봐 너무나 좋아
+니가 날 혹시 안 좋아할까봐
+혼자 얼마나 애 태운지 몰라
+그런데 니가 날 사랑한다니
+어머나 다시 한 번 말해봐
+Tell me tell me
+tell tell tell tell tell tell me
+나를 사랑한다고 날 기다려 왔다고
+Tell me tell me
+tell tell tell tell tell tell me
+내가 필요하다 말해 말해줘요
+Tell me tell me
+tell tell tell tell tell tell me
+자꾸만 듣고 싶어 계속 내게 말해줘
+Tell me tell me
+tell tell tell tell tell tell me
+꿈이 아니라고 말해 말해줘요
+어쩜 내 가슴이 이렇게 뛰니
+가슴이 정말 터질 것 같아
+니가 날 볼 때면
+전기에 감전된 사람처럼 전기가 올라
+얼마나 오래 기다린지 몰라
+얼마나 오래 꿈 꿨는지 몰라
+그런데 니가 날 사랑한다니 어머나
+다시 한 번 말해봐
+Tell me tell me tell me you
+Want me want me want me too
+Tell me tell me tell me you
+Love me too Love me too
+얼마나 오래 기다린지 몰라
+얼마나 오래 꿈 꿨는지 몰라
+그런데 니가 날 사랑한다니
+어머나, 다시 한 번 말해봐
+Tell me tell me
+tell tell tell tell tell tell me
+tell tell tell tell tell tell me
+Tell me", 5);
+
+-- 빅뱅 - 뱅뱅뱅
+insert into music(music_title, singer, video_id)
+values ('뱅뱅뱅', '빅뱅', '2ips2mM7Zqw');
+insert into lyrics(music_lyrics, music_id)
+values ("난 깨어나 까만 밤과 함께
+다 들어와 담엔 누구 차례
+한 치 앞도 볼 수 없는 막장 게릴라
+경배하라 목청이 터지게
+찌질한 분위기를 전환해
+광기를 감추지 못하게 해
+남자들의 품위 여자들의 가식
+이유 모를 자신감이 볼만해
+난 보란 듯이 너무나도 뻔뻔히
+니 몸속에 파고드는 알러지
+이상한 정신에 술렁이는 천지
+오늘 여기 무법지
+난 불을 질러
+심장을 태워
+널 미치게 하고 싶어
+B I G we bang like this 모두 다 같이
+총 맞은 것처럼
+BANG BANG BANG
+BANG BANG BANG
+빵야 빵야 빵야
+BANG BANG BANG
+BANG BANG BANG
+빵야 빵야 빵야
+다 꼼짝 마라 다 꼼짝 마
+다 꼼짝 마라 다 꼼짝 마
+오늘 밤 끝장 보자 다 끝장 봐
+오늘 밤 끝장 보자
+빵야 빵야 빵야
+널 데려가 지금 이 순간에
+새빨간 저 하늘이 춤출 때
+돌고 돌아 너와 나 이곳은 Valhalla
+찬양하라 더 울려 퍼지게
+We go hard 불침번 밤새 달려 축지법
+이 노래는 꼭짓점 신이나 불러라 신점
+큰 비명소리는 마성의 멜로디 검은 독기의 연결 고리
+사방 팔방 오방 가서 푸는 고삐
+난 불을 질러
+네 심장을 태워
+널 미치게 하고 싶어
+BIG Yea we bang like this 모두 다 같이
+총 맞은 것처럼
+BANG BANG BANG
+BANG BANG BANG
+빵야 빵야 빵야
+BANG BANG BANG
+BANG BANG BANG
+빵야 빵야 빵야
+다 꼼짝 마라 다 꼼짝 마
+다 꼼짝 마라 다 꼼짝 마
+오늘 밤 끝장 보자 다 끝장 봐
+오늘 밤 끝장 보자
+빵야 빵야 빵야
+Ready or not
+Yea we dont give a what
+Ready or not
+Yea we dont give a what
+Lets go 남자들은 위로
+여자들은 get low
+당겨라 bang bang bang
+let the bass drum go
+남자들은 위로
+여자들은 get low
+당겨라 bang bang bang
+let the bass drum go", 6);
+
+-- 싸이 - 예술이야
+insert into music(music_title, singer, video_id)
+values ('예술이야', '싸이', '1cKc1rkZwf8');
+insert into lyrics(music_lyrics, music_id)
+values ("너와 나 둘이 정신없이 가는 곳 정처 없이 가는 곳 정해지지 않은 곳
+거기서 우리 서로를 재워주고 서로를 깨워주고 서로를 채워주고
+EXCUSE ME 잠시만 아직까진 우린 남 하지만 조만간 중독성을 자랑하는 장난감
+지금 이 느낌적인 느낌이 통하는 느낌 녹아버릴 아이스크림
+지금이 우리에게는 꿈이야 너와 나 둘이서 추는 춤이야
+기분은 미친 듯이 예술이야 우웨오 우웨오 우웨오
+하늘을 날아가는 기분이야 죽어도 상관없는 지금이야
+심장은 터질 듯이 예술이야 우웨오 우웨오 우웨오
+예술이야 예술이야 예술이야
+이런 날이 올 줄이야 예술이야 예술이야
+너와 나 둘이 밤새 잔을 부딪혀 밤새 뺨을 부비며 밤새도록 둘이서
+눈이 점점 풀린다 다리도 따라 풀린다 끌린다
+너에 대한 수수께끼가 풀린다
+지금 이 춤에 너의 가빠진 숨에 수줍음에
+YOU KNOW WHAT I MEAN
+지금이 우리에게는 꿈이야 너와 나 둘이서 추는 춤이야
+기분은 미친 듯이 예술이야
+우웨오 우웨오 우웨오
+하늘을 날아가는 기분이야 죽어도 상관없는 지금이야
+심장은 터질 듯이
+예술이야 우웨오 우웨오 우웨오
+여기서 우리 같은 것을 즐기고 같은 것을 느끼고 웃다가 흐느끼고
+아가씨 만나기 전엔 난 건달 미녀 만나기 전엔
+야수였던 날 바꿔 버렸어 내가 찾던 사람 너였어 예술이였어", 7);
+
+-- EXO - 으르렁
+insert into music(music_title, singer, video_id)
+values ('으르렁', 'EXO', 'I3dezFzsNss');
+insert into lyrics(music_lyrics, music_id)
+values ("yo okay 나 혹시 몰라 경고하는데 지금 위험해
+자꾸 나를 자극하지마 나도 날 몰라
+숨이 자꾸 멎는다 네가 날 향해 걸어온다
+나를 보며 웃는다 너도 내게 끌리는지
+눈앞이 다 캄캄해 네가 뚫어져라 쳐다볼 땐
+귓가에 가까워진 숨소리 날 미치게 만드는 너인걸
+아무도 널 못 보게 품에 감추고 싶어
+널 노리는 시선들 내 안에 일어난 거센 소용돌이
+검은 그림자 내 안에 깨어나 널 보는 두 눈에 불꽃이 튄다
+그녀 곁에서 모두 다 물러나 이젠 조금씩 사나워진다
+나 으르렁 으르렁 으르렁 대 나 으르렁 으르렁 으르렁 대
+나 으르렁 으르렁 으르렁 대 너 물러서지 않으면 다쳐도 몰라
+날이 선 눈빛과 베일듯한 긴장감 지금 검색 중이야 너의 주위를
+넌 그냥 그대로 있어 나만을 바라보면서 절대 널 보내지 않아 두고 봐 babe
+흐린 공간속에서 선명하게 빛나는
+널 노리는 시선들 내 안에 울리는 경보 울림소리
+검은 그림자 내 안에 깨어나 널 보는 두 눈에 불꽃이 튄다
+그녀 곁에서 모두 다 물러나 이젠 조금씩 사나워진다
+나 으르렁 으르렁 으르렁 대 나 으르렁 으르렁 으르렁 대
+나 으르렁 으르렁 으르렁 대 너 물러서지 않으면 다쳐도 몰라
+E X O 또 다른 늑대들이 볼세라 너무나 완벽한 내 여자라
+품속엔 부드럽게 너를 안고 너만을 위해서 나는 난폭해지고
+결국엔 강한 자가 얻게 되는 미인 자리가 없으니까 그냥 돌아가 I win
+가능성 제로야 닳으니까 그만 봐 그녀를 넘본다면 나를 먼저 넘어봐", 8);
+
+-- IU - 마쉬멜로우
+insert into music(music_title, singer, video_id)
+values ('마쉬멜로우', 'IU', 'slT80EySpKk');
+insert into lyrics(music_lyrics, music_id)
+values ("말랑 말랑 말랑 말랑 해
+넌 특별해 완벽해 비교 분석 해봐도 다른 애들과는 다르지 달라 달라 Boy
+난 한심해 부족해 너에게서 떨어져 바라보기만 하는 바보지
+내가 왜 이럴까 너만 생각하다 라랄라라 랄라 날아 올라
+바로 그때 어떡해 뚜벅 뚜벅 네가 걸어와 Blah Blah 얘기를 하지
+무슨 말을 해도 들리지가 않아 째깍 째깍 시간이 STOP
+마쉬멜로우 마쉬멜로우 달콤해서 너무 좋아
+마쉬멜로우 마쉬멜로우 사랑이란 이런 걸까
+말랑 말랑 말랑해 너무 너무나 말랑해 Girl Girl baby girl Clap Clap let it go
+젤리처럼 통통해 쿠키처럼 촉촉해 Boy Boy baby boy boy 마쉬멜로우
+난 소심해 고민해 너를 떠올릴 때면 얼굴이 새빨게 지는 아이인 걸
+내가 왜 이럴까 너만 상상하다 라랄라라 랄라 날아 올라
+바로 그때 어떡해 두근 두근 네가 다가와 Say what
+사귀자고 대쉬를 하지 어떡해 어떡해
+너의 목소리만 듣고 싶어 정말 가만 가만 모두 다 쉿
+마쉬멜로우 마쉬멜로우 달콤해서 너무 좋아
+마쉬멜로우 마쉬멜로우 사랑이란 이런 걸까
+말랑 말랑 말랑해 너무 너무나 말랑해
+Girl Girl baby girl Clap Clap let it go
+젤리처럼 통통해 쿠키처럼 촉촉해 Boy Boy baby boy boy
+꿈을 꿨나봐 눈을 뜨니까 거짓말처럼 다 사라져 널 만날 거야 다시 잘 거야 몰라
+마쉬멜로우 마쉬멜로우 달콤해서 너무 좋아
+마쉬멜로우 마쉬멜로우 사랑이란 이런 걸까
+말랑 말랑 말랑해 너무 너무나 말랑해 Girl Girl baby girl Clap Clap let it go
+젤리처럼 통통해 쿠키처럼 촉촉해 Boy Boy baby boy boy
+마쉬멜로우", 9);
+
+-- SISTAR - Loving U
+insert into music(music_title, singer, video_id)
+values ('Loving U', 'SISTAR', 'oL2AlXWVbKU');
+insert into lyrics(music_lyrics, music_id)
+values ("Hey 참 이상해 정말 strange 이런 적 없었는데 널 볼때마다 나 두근두근 떨리는게
+When I see your face 혼자 막 상상을 해 나 표정 관리 안돼 미칠 것 같아 또 니가 니가 보고 싶어서
+I wanna say woo woo woo woo
+너 때문에 그래 먼저 말하긴 싫은데 내 입가에 맴돌아
+Loving U U 나 어떡해 what should I do
+조절이 안돼 내 맘 고장 나버린 heart 정신 못 차릴 만큼 Boy I am falling in love with U U 나 어떡해 what should I do
+널 놓치면 난 안돼 사랑한다고 Woo woo woo woo woo Woo woo woo woo woo woo Baby I am in love with you
+ok lets talk about love 모두 휘파람을 불며 어머나 나를 꼬시려고 했던 남자들 이젠 모두 안녕
+내 애교섞인 목소리는 오빠만 부를래 오빠야 어쩌면 좋아 내가 사랑에 빠졌나 봐 Woo woo woo woo
+너 때문에 그래 먼저 말하긴 싫은데 내 입가에 맴돌아 Loving U U 나 어떡해 what should I do
+조절이 안돼 내 맘 고장 나버린 heart 정신 못 차릴 만큼 Boy I''m falling in love with U U 나 어떡해 what should I do
+널 놓치면 난 안돼 사랑한다고 Woo woo woo woo woo Woo woo woo woo Baby I am in love with you
+애타는 내 맘 너무 기나긴 밤 숨겨왔던 너를 향한 말
+사랑해 Baby I am in love with U U 나 어떻게 해 What should I do
+조절이 안돼 내 맘 고장 나버린 heart 정신 못 차릴 만큼 Boy I am falling in love with U U 나 어떡해 what should I do
+널 놓치면 난 안돼 사랑한다고 Woo woo woo woo woo Woo woo woo woo woo woo Baby I am in love with you
+Hello Hello 눈이 마주치면 L O L O V E 나는 말해 멜로 멜로 우린 L O L O V E L O L O V E I am only think about U", 10);
+
+-- 동방신기 - 풍선
+insert into music(music_title, singer, video_id)
+values ('풍선', '동방신기', 'rdTVb64pfMs');
+insert into lyrics(music_lyrics, music_id)
+values ("지나가버린 어린 시절엔 풍선을 타고 날아가는 예쁜 꿈도 꾸었지
+빨간 풍선이 하늘을 날면 내 마음에도 아름다운 기억들이 생각나
+내 어릴 적 꿈은 빨간 풍선을 타고 하늘 높이 날으는 사람
+그 조그만 꿈을 잊어버리고 산 건 내가 너무 커버렸을 때
+하지만 괴로울 땐 아이처럼 뛰어 놀고 싶어
+조그만 나의 꿈들을 풍선에 가득 싣고
+지나가버린 어린 시절엔 풍선을 타고
+날아가는 예쁜 꿈도 꾸었지
+빨간 풍선이 하늘을 날면 내 마음에도 아름다운 기억들이 생각나
+왜 하늘을 보면 나는 눈물이 날까 그것 조차 알 수 없잖아
+왜 어른이 되면 잊어버리게 될까 조그맣던 아이 시절을
+때로는 나도 그냥 하늘 높이 날아가고 싶어 잊었던 나의 꿈들과 추억을 가득 싣고
+지나가버린 어린 시절엔 풍선을 타고 날아가는 예쁜 꿈도 꾸었지
+빨간 풍선이 하늘을 날면 내 마음에도 아름다운 기억들이 생각나
+랄라라라라 세월이 흘러도
+랄라라라라 잊어버려도
+랄라라라라 빨간 풍선에 담을 수 있을까
+지나가버린 어린 시절엔 풍선을 타고 날아가는 예쁜 꿈도 꾸었지
+빨간 풍선이 하늘을날면 내 마음에도 아름다운 기억들이 생각나", 11);
+
+-- BLACKPINK - How you like that
+insert into music(music_title, singer, video_id)
+values ('How you like that', 'BLACKPINK', 'ioNng23DkIM');
+insert into lyrics(music_lyrics, music_id)
+values ("보란 듯이 무너졌어
+바닥을 뚫고 저 지하까지
+옷 끝자락 잡겠다고
+저 높이 두 손을 뻗어봐도
+다시 캄캄한 이곳에 light up the sky
+네 두 눈을 보며 I will kiss you goodbye
+실컷 비웃어라 꼴좋으니까
+이제 너희 하나 둘 셋
+how you like that
+You gon like that that that that that
+How you like that
+How you like that that that that that
+Now look at you now look at me
+Look at you now look at me
+Look at you now look at me
+How you like that
+Now look at you now look at me
+Look at you now look at me
+Look at you now look at me
+How you like that
+Your girl need it all and thats a hundred
+백 개 중에 백 내 몫을 원해
+Karma come and get some
+딱하지만 어쩔 수 없잖아
+Whats up I am right back
+방아쇠를 cock back
+Plain Jane get hijacked
+Dont like me Then tell me how you like that
+더 캄캄한 이곳에 shine like the stars
+그 미소를 띠며 I will kiss you goodbye
+실컷 비웃어라 꼴좋으니까
+이제 너희 하나 둘 셋
+how you like that
+You gon like that that that that that
+How you like that
+How you like that that that that that
+Now look at you now look at me
+Look at you now look at me
+Look at you now look at me
+How you like that
+Now look at you now look at me
+Look at you now look at me
+Look at you now look at me
+How you like that
+날개 잃은 채로 추락했던 날
+어두운 나날 속에 갇혀 있던 날
+그때쯤에 넌 날 끝내야 했어
+Look up in the sky its a bird its a plane
+Bring out your boss bish
+BLACKPINK
+How you like that
+You gon like that
+How you like that", 12);
+
+-- TWICE - Cheer up
+insert into music(music_title, singer, video_id)
+values ('Cheer up', 'TWICE', 'c7rCyll5AeY');
+insert into lyrics(music_lyrics, music_id)
+values ("매일 울리는 벨벨벨
+이젠 나를 배려 해줘
+배터리 낭비하긴 싫어
+자꾸만 봐 자꾸 자꾸만 와
+전화가 펑 터질 것만 같아
+몰라 몰라 숨도 못 쉰대
+나 때문에 힘들어
+쿵 심장이 떨어진대 왜
+걔 말은 나 너무 예쁘대
+자랑 하는건 아니구
+아 아까는 못 받아서 미안해
+친구를 만나느라 shy shy shy
+만나긴 좀 그렇구 미안해
+좀 있다 연락할게 later
+조르지마 얼마 가지 않아
+부르게 해줄게 Baby
+아직은 좀 일러 내 맘 같긴 일러
+하지만 더 보여줄래
+CHEER UP BABY
+CHEER UP BABY
+좀 더 힘을 내
+여자가 쉽게 맘을 주면 안돼
+그래야 니가 날 더 좋아하게 될걸
+태연하게 연기할래 아무렇지 않게
+내가 널 좋아하는 맘 모르게
+just get it together and then baby CHEER UP
+안절부절 목소리가 여기까지 들려
+땀에 젖은 전화기가 여기서도 보여
+바로 바로 대답하는 것도 매력 없어
+메시지만 읽고 확인 안 하는 건 기본
+너무 심했나 boy
+이러다가 지칠까 봐 걱정되긴 하고
+안 그러면 내가 더
+빠질 것만 같어 빠질 것만 같어
+아 답장을 못해줘서 미안해
+친구를 만나느라 shy shy shy
+만나긴 좀 그렇구 미안해
+좀 있다 연락할게 later
+조르지마 어디 가지 않아
+되어줄게 너의 Baby
+너무 빨린 싫어 성의를 더 보여
+내가 널 기다려줄게
+나도 니가 좋아 상처 입을까 봐
+걱정되지만 여자니까 이해해주길
+속 마음 들킬 까봐 겁이나
+지금처럼 조금만 더 다가와
+그리 오래 걸리진 않아
+just get it together
+and then baby CHEER UP
+Be a man a real man
+gotta see u love me
+like a real man", 13);
+
+-- 샤이니 - Lucifer
+insert into music(music_title, singer, video_id)
+values ('Lucifer', '샤이니', 'Dww9UjJ4Dt8');
+insert into lyrics(music_lyrics, music_id)
+values ("숨을 곳도 찾지 못해 나는 피하려고 애써봐도
+거부조차 할 수 없는 네게 갇혀버린 나
+사랑이었다면 정말 사랑했던 거라면
+내게 이러지는 말아
+Her whisper is the LUCIFER
+LUCIFER
+나를 묶고 가둔다면 사랑도 묶인 채
+미래도 묶인 채 커질 수 없는데
+자유롭게 비워놓고 바라봐
+오직 너만 채울게 너만 가득 채울게
+거부 할 수 없는 너의 마력은 LUCIFER
+거부 할 수 없는 너의 마법은 LUCIFER
+다가서면 너는 마치 천사 같은 얼굴로
+나를 사는 이유라 말하고
+너를 처음 봤을 때 짧은 순간 멈춰버렸지
+누가 마치 내 심장을 꽉 쥔 채
+놓지 않는 것처럼 아직까지도
+너는 그렇게
+내 맘을 다
+니가 없으면 내 맘이 다 타버리게 만든다
+나를 묶고 가둔다면 사랑도 묶인 채
+미래도 묶인 채 커질 수 없는데
+자유롭게 비워놓고 바라봐
+오직 너만 채울게 너만 가득 채울게
+Loverhollic
+Loverhollic
+너와 같이 나눈 사랑얘기들
+나랑 같은 곳을 바라보는 너
+우리 더 이상은 완벽해질 수 없다고 느꼈을 때
+나만 쳐다봐
+너는 더
+나만 바라봐
+모든 게
+언제부턴가 조금씩 잘못된 것 같아 이상한 너
+날 알던 사람들 모두 하나 둘씩 곁을 떠나
+난 가진 게 너뿐이고
+나를 묶고 가둔다면 사랑도 묶인 채
+미래도 묶인 채 커질 수 없는데
+자유롭게 비워놓고 바라봐
+오직 너만 채울게 너만 가득 채울게
+마치 유리성에 갇혀버린 삐에로만 된 것 같아
+절대 만족 없는 너를 위해 춤을 추는 나
+훤히 들여다보고 뇌를 만져 바보 된 것 같아
+난 점점 네게 끌려가는 것만 같은데
+Loverhollic
+Loverhollic
+니가 미운 건 아냐 싫단 것도 아냐
+단지 그런 눈빛이 부담일 뿐
+어디 안가 이렇게
+난 너만 바라보고 기다려 왔잖아
+너의 눈빛이 날 사로잡다
+날이 지날수록 날카로워졌다
+너의 집착에 지쳤다
+많이 베었다 맘에 피가 난다
+나 쓰러질 쯤 되면 다가와서
+천사같이 사랑해란 말
+누가 진짜 너였는지
+알다가도 헷갈리게 만든다
+나를 묶고 가둔다면 사랑도 묶인 채
+미래도 묶인 채 커질 수 없는데
+자유롭게 비워놓고 바라봐
+오직 너만 채울게 너만 가득 채울게
+마치 유리성에 갇혀버린 삐에로만 된 것 같아
+나를 냅둬 자유로워 질 때
+너를 진짜 사랑할 수 있고
+훤히 들여다보고 뇌를 만져 바보 된 것 같아
+나를 냅둬 질리게도 말고
+너를 진짜 바라볼 수 있게
+나를 묶고 가둔다면 사랑도 묶인 채
+미래도 묶인 채 커질 수 없는데
+Loverhollic
+Loverhollic
+거부할 수 없는 너의 마력은 LUCIFER", 14);
